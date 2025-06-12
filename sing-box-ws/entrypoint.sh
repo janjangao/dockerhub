a76cee2f6b0ec7e9625c3fdb91a3129c5cced295
jian.gao <focussellingcute30years@gmail.com>
2025-06-12 15:57:41 +0800

    feat: optimize sing-box-ws

--- sing-box-ws/entrypoint.sh
+++ sing-box-ws/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-sed -i "s/PORT/$PORT/g" /etc/config/sing-box/config.json
-sed -i "s/PASSWORD/$PASSWORD/g" /etc/config/sing-box/config.json
-exec "$@"
+
+set -e
+
+sed -i "s|\${PASSWORD}|${PASSWORD}|g" /etc/config/sing-box/config.json
+sed -i "s|\${PORT}|${PORT}|g" /etc/config/traefik/traefik.yml
+
+echo "[INFO] Checking sing-box config..."
+sing-box check -c /etc/config/sing-box/config.json || exit 1
+
+sing-box run -c /etc/config/sing-box/config.json &
+whoami --port=8080 &
+
+echo "[INFO] sing-box and whoami started, launching traefik..."
+exec traefik --configFile=/etc/config/traefik/traefik.yml
