7d9aee5075fe675f356cde836ecc5a7949714b7a
jian.gao <focussellingcute30years@gmail.com>
2025-06-13 22:19:40 +0800

    feat: change sing-box structure

--- sing-box-ws/entrypoint.sh
+++ sing-box-ws/entrypoint.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-sed -i "s|\${PASS}|${PASS}|g" /etc/config/sing-box/config.json
+mkdir -p /etc/config/
+cp -r /config/* /etc/config/
+
+sed -i "s|\${UUID}|${UUID}|g" /etc/config/sing-box/config.json
 sed -i "s|\${PORT}|${PORT}|g" /etc/config/traefik/traefik.yml
 
 echo "[INFO] Checking sing-box config..."
 sing-box check -c /etc/config/sing-box/config.json || exit 1
 
 sing-box run -c /etc/config/sing-box/config.json &
-whoami --port=8080 &
+whoami --port=8000 &
 
 echo "[INFO] sing-box and whoami started, launching traefik..."
 exec traefik --configFile=/etc/config/traefik/traefik.yml
