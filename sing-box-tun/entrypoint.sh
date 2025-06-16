448232c419b321433d098794e06a10a2536d7a67
jian.gao <focussellingcute30years@gmail.com>
2025-06-16 17:07:25 +0800

    feat: add mini config

--- sing-box-tun/entrypoint.sh
+++ sing-box-tun/entrypoint.sh
@@ -5,14 +5,11 @@ set -e
 if [ ! -f /etc/config/sing-box/config.json ]; then
     mkdir -p /etc/config/sing-box
     cp -r /config/sing-box/config.json /etc/config/sing-box/config.json
-    sed -i "s|\${UUID}|${UUID}|g" /etc/config/sing-box/config.json
 fi
 if [ ! -d /etc/config/sing-box/rule-sets ]; then
     mkdir -p /etc/config/sing-box/rule-sets
     cp -r /config/sing-box/rule-sets/* /etc/config/sing-box/rule-sets/
 fi
-
-sed -i "s|\${UUID}|${UUID}|g" /etc/config/sing-box/config.json
 
 echo "[INFO] Checking sing-box config..."
 sing-box check -c /etc/config/sing-box/config.json || exit 1
